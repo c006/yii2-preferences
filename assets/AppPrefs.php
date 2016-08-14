99012bce49e9f6ae159a74593d92f2f849c83cb7
Jon Chambers <jchambers.dev@gmail.com>
2016-08-13 23:40:30 -0700

    updates

--- assets/AppPrefs.php
+++ assets/AppPrefs.php
@@ -2,11 +2,26 @@
 namespace c006\preferences\assets;
 
 
+use c006\preferences\models\Preferences;
+
 class AppPrefs
 {
 
- static public function getPreference() {
+    /**
+     * @param $key
+     * @return string
+     */
+    static public function getPreference($key)
+    {
 
- }
+        $model = Preferences::find()
+            ->where(['key' => $key])
+            ->asArray()
+            ->one();
+
+        return (sizeof($model)) ? $model['value'] : FALSE;
+
+    }
+
 
 }
